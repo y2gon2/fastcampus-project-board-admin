0de8a143596f0849f403298a7fd51ab2bbed5e25
gon <y2gon2@gmail.com>
2023-10-15 13:21:17 +0900

    #19 - 관리자 계정 관리 view 구현
    AdminAccountController / AdminAccountControllerTest
    -  미 구현된 관리자 계정 페이지 controller 와 기본 get mapping test 추가
    
    members.html /  members.th.xml
    - 관리자 계정 관리 view 를 기존 template code 를 적용하여 생성.
    - 본문 table 의 경우, 다른 페이지들과 달리 adminLTE  table template 중 페이지에서 계정 추가, 수정, 삭제가 가능한  jsgrid 를 적용함.

---
+++ src/test/java/com/fastcampus/projectboardadmin/controller/AdminAccountControllerTest.java
@@ -0,0 +1,37 @@
+package com.fastcampus.projectboardadmin.controller;
+
+import com.fastcampus.projectboardadmin.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View Controller - Admin Account")
+@Import(SecurityConfig.class)
+@WebMvcTest(AdminAccountController.class)
+class AdminAccountControllerTest {
+
+    private final MockMvc mvc;
+
+    public AdminAccountControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[View][GET] 관리자 계정 페이지 - 정상 호출")
+    @Test
+    void givenNothing_whenRequestingAdminAccountView_thenWorksFine() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/admin/members"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("admin/members"));
+    }
+}
