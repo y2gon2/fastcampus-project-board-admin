6657f0400ceb7c463e521faaa6e9690e736635da
gon <y2gon2@gmail.com>
2023-10-16 18:01:28 +0900

    #40 - UserAccount Entity & DTO 혼용 사용 정리
    초기 설정에서 관리자 계정 정보 table 명을 [게시판 프로젝트] 에서 받아오는 일반 회원 계정 table 과 같아, 혼용되어 구현되고 있었다.
    이를 분리하여 [게시판 프로젝트 - 일반 회원 계정] 은 그대로 UserAccount 사용하고, 관리자 계정을 AdminAccount 로 수정 및 상황에 맞게 객체 사용을 정리하였다.
    아울러, 일반회원 정보에서 받을 수 없는 RoleType 정보와  보안 상 일반 계정에서 받을 필요가 없는 password 정보는 UserAccountDto 에서 제거하였다.
    UserAccount Entity 는 우선 삭제 하였다.

--- src/main/java/com/fastcampus/projectboardadmin/dto/UserAccountDto.java
+++ src/main/java/com/fastcampus/projectboardadmin/dto/UserAccountDto.java
@@ -1,18 +1,9 @@
 package com.fastcampus.projectboardadmin.dto;
 
-import com.fastcampus.projectboardadmin.domain.UserAccount;
-import com.fastcampus.projectboardadmin.domain.constant.RoleType;
-
 import java.time.LocalDateTime;
-import java.util.Set;
 
-/**
- * DTO for {@link com.fastcampus.projectboardadmin.domain.UserAccount}
- */
 public record UserAccountDto(
         String userId,
-        String userPassword,
-        Set<RoleType> roleTeyps,
         String email,
         String nickname,
         String memo,
@@ -23,74 +14,25 @@ public record UserAccountDto(
 ) {
     public static UserAccountDto of(
             String userId,
-            String userPassword,
-            Set<RoleType> roleTypes,
             String email,
             String nickname,
             String memo
     ) {
-        return UserAccountDto.of(
-                userId,
-                userPassword,
-                roleTypes,
-                email,
-                nickname,
-                memo,
-                null,
-                null,
-                null,
-                null
+        return new UserAccountDto(
+                userId, email, nickname, memo, null, null, null, null
         );
     }
 
     public static UserAccountDto of(
             String userId,
-            String userPassword,
-            Set<RoleType> roleTypes,
             String email,
             String nickname,
             String memo,
             LocalDateTime createdAt,
             String createdBy,
             LocalDateTime modifiedAt,
-            String modifiedBy
-    ) {
-        return new UserAccountDto(
-                userId,
-                userPassword,
-                roleTypes,
-                email,
-                nickname,
-                memo,
-                createdAt,
-                createdBy,
-                modifiedAt,
-                modifiedBy);
+            String modifiedBy) {
+        return new UserAccountDto(userId, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
     }
 
-    public static UserAccountDto from(UserAccount entity) {
-        return new UserAccountDto(
-                entity.getUserId(),
-                entity.getUserPassword(),
-                entity.getRoleTypes(),
-                entity.getEmail(),
-                entity.getNickname(),
-                entity.getMemo(),
-                entity.getCreatedAt(),
-                entity.getCreatedBy(),
-                entity.getModifiedAt(),
-                entity.getModifiedBy()
-                );
-    }
-
-    public UserAccount toEntity() {
-        return UserAccount.of(
-                userId,
-                userPassword,
-                roleTeyps,
-                email,
-                nickname,
-                memo
-        );
-    }
 }
