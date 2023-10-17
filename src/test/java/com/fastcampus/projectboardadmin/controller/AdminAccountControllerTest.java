dbfc4b21a58af10647ef6f190e8df642a196c7ac
gon <y2gon2@gmail.com>
2023-10-17 21:19:12 +0900

    #24 - 관리자 계정 관리  Test 구현
    관리자 계정 controller, service 에서 구현되어야 할 Business Logic 을 예상하여 우선,  해당 요청과 결과를 예상하여 작성한 Test 를 작성
    Test 작성에 필요한 controller, service method skeleton, DTO,  RoleType 추가 사항 등이 반영됨.
    
    AdminAccountController
     - 관리자 계정 관리 화면 요청
     - 화면에서 계정 정보 수정이 가능하므로 api 요청 (관리자 정보 요청, 삭제 요청) . Http  본문으로 부터 응답을 바로 받을 수 있도록 @ResponseBody 추가
    
     AdminAccountService
     - 관리자 정보 찾기, 저장,  전체 관리자 계정 리스트 요청, 계정 삭제
    
    AdminAccountControllerTest
     - controller method 작업에 대한 결과 확인 test
     - 다른 controller test 와 달리 test 에서 Mocking 되는 AdminAccountService 가 Spring Security 의 작업에 영향을 준다. 때문에 이미 TestSecurityConfig 에서 해당 service Mock 이 존재 하므로  이것을 사용할 수 없다. (import 시 Duplicate mock definition error 발생) 따라서  SecurityConfig.class 를 import 하고 test 전 security 조건을 설정하는 코드를 내부적으로 구현해 준다.
    
    AdminAccountServiceTest
     - 다음의 조건을 Test 함
     - ID 조회 후, 있으면 해당 정보를 반환 / 없으면 빈 Optional 을 반환 / 관리자 정보를 입력하면 가입 / 전체 관리자 정보 조회 / 관리자 정보 삭제
    
    RoleType
     - 기존 getter  로 얻을 수 있는 정보는 ROLE_USER 와 같이 spring securiy 에서 필요로 하는 정보만을 담고 있었으나, 화면 출력용 (한글) 정보를 getter 할 수 있도록 해당 내용을 추가함.
    
    AdminAccountResponse
     -  관리자 계정 API 의 응답 객체 설계

--- src/test/java/com/fastcampus/projectboardadmin/controller/AdminAccountControllerTest.java
+++ src/test/java/com/fastcampus/projectboardadmin/controller/AdminAccountControllerTest.java
@@ -1,17 +1,38 @@
 package com.fastcampus.projectboardadmin.controller;
 
 import com.fastcampus.projectboardadmin.config.SecurityConfig;
+import com.fastcampus.projectboardadmin.config.TestSecurityConfig;
+import com.fastcampus.projectboardadmin.domain.constant.RoleType;
+import com.fastcampus.projectboardadmin.dto.AdminAccountDto;
+import com.fastcampus.projectboardadmin.service.AdminAccountService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.mockito.BDDMockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+// @Import(TestSecurityConfig.class) 적용시 문제점
+// test 에서 Spring Security 인증 과정을 매번 수행하지 않도록
+// 미리 계정을 생성하고 인증을 통과하는 작업을 완료해 놓은 TestSecurityConfig.class 를
+// import 하여 사용하였다.
+// 그런데 해당 test 의 대상이 곧 인증 대상이므로, AdminAccount가  두번 정의되는 문제가 발생한다.
+// (Error: Duplicate mock definition)
+// 따라서 실제 application class (SecurityConfig.class) 를 사용하고, 관련 인증 처리를 모두 해주어야 한다.
 @DisplayName("View Controller - Admin Account")
 @Import(SecurityConfig.class)
 @WebMvcTest(AdminAccountController.class)
@@ -19,13 +40,34 @@ class AdminAccountControllerTest {
 
     private final MockMvc mvc;
 
+    @MockBean private AdminAccountService adminAccountService;
+
     public AdminAccountControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
 
+    // 앞에서 언급한대로 TestSecurtiyConfig.class import 를 쓸 수 없기 때문에 (Duplicate mock definition 발생)
+    // security 설정을 내부에서 진행
+    @BeforeTestMethod
+    public void securitySetup() {
+        given(adminAccountService.searchUser(anyString()))
+                .willReturn(Optional.of(createAdminAccountDto()));
+
+        // 가입용?
+        given(adminAccountService.saveUser(
+                anyString(),
+                anyString(),
+                anySet(),
+                anyString(),
+                anyString(),
+                anyString()
+        )).willReturn(createAdminAccountDto());
+    }
+
+    @WithMockUser(username = "tester", roles = "USER")
     @DisplayName("[View][GET] 관리자 계정 페이지 - 정상 호출")
     @Test
-    void givenNothing_whenRequestingAdminAccountView_thenWorksFine() throws Exception {
+    void givenAuthorizedUser_whenRequestingAdminMembersView_thenReturnsAdminMembersView() throws Exception {
         // Given
 
         // When & Then
@@ -33,5 +75,49 @@ class AdminAccountControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("admin/members"));
+    }
+
+    @WithMockUser(username = "tester", roles = "USER")
+    @DisplayName("[data][GET] 관리자 리스트 - 정상 호출")
+    @Test
+    void givenAuthorizedUser_whenRequestingAdmin_thenReturnsAdmin() throws Exception {
+        // Given
+        given(adminAccountService.users()).willReturn(List.of());
+
+        // When & Then
+        mvc.perform(get("/api/admin/members"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+        then(adminAccountService).should().users();
+    }
+
+    @WithMockUser(username = "tester", roles = "MANAGER")
+    @DisplayName("[data][DELETE] 관리자 정보 삭제 - 정상 호출")
+    @Test
+    void givenAuthorizedUser_whenDeletingAdminAccount_thenDeletesAdminAccount() throws Exception {
+        // Given
+        String username = "uno";
+        willDoNothing().given(adminAccountService).deleteUser(username);
+
+        // When & Then
+        mvc.perform(
+                delete("/api/admin/members/" + username)
+                        .with(csrf())
+        )
+                .andExpect(status().isNoContent());
+        then(adminAccountService).should().deleteUser(username);
+
+    }
+
+    // -------------------- Fixture for Test --------------------
+    private AdminAccountDto createAdminAccountDto() {
+        return AdminAccountDto.of(
+                "unoTest",
+                "pw",
+                Set.of(RoleType.USER),
+                "uno-test@email.com",
+                "uno-test",
+                "test memo"
+        );
     }
 }
