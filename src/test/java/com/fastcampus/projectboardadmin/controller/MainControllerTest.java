84491e288d6d9f52d4a81bddbd42c47108f6599f
gon <y2gon2@gmail.com>
2023-10-22 16:17:22 +0900

    #61 - web 방문자 수 counting logic 및 화면 출력을 위한  code 작성 및 연관 Test 수정
    방문자 수 계산 및 표시 관련 학습을 위해, 관리자 application 에서는 사실 불필요하지만, 관련 기능을 넣어준다.
    학습을 위해, view 의 각 페이지를 열 때마다 전체 방문자 수가 증가하도록 구현하였다.
    
    VisitCounterControllerAdvice
     - @ControllerAdvice annotation 을 적용하여, controller component 들이 실행할 때, 공통적으로 적용할 수 있는 component 를 생성해 준다. 주로  client 의 잘못된 요청에 대한 error 처리  @ExceptionHandler 나 모든 view 에 대한 model attribute 를 적용하기 위해 사용된다. 따라서 각 page 에 모두 `방문횟수` 를 반영하기 위한 model attribute 를 정의 하고, 이와 관련된 business logic 을 구현할 VisitCounterService method 를 명시해 준다.
    
    VisitCounterService
     - 현 구현에서 방문자 수에 대한 영속성 부여 작업은 없이 browser session 이 유지되는 내에서의  방문횟수만 기록한다.
     - 방문횟수는 spring actuator 에서 제공하는 micrometer 라이브러리 기능을 사용한다.  이중 MeterRegistry class 를 사용하면 java applicatoin 의 성능 metrics 를 측정할 수 있으며,  application 으로의 요청 횟수(http.server.reqeuests) 중 작성자가 의도한 page view 요청 endpoint 들로 filtering 하여 앞에서 설계한 대로 방문횟수를 count 할 수 있다.
     - 다만, 최초 appilcation 을 실행했을 때, 해당 방문횟수는 기록될 수 없으므로 error 를 발생시킨다. 따라서 이에 대한 error catch 처리를 함께 넣어준다.
    
    GlobalControllerConfig
     - @ControllerAdvice 적용 component 가 추가됨에 따라,  모든 Test component 들도 해당 bean 생성이 영향을 받게 된다. 따라서 test 에서는 해당 bean 생성을 우회해서 직접 해당 service 에 접근하여, test 에 필요한 결과 값을 받을 있도록 별도의 설정이 필요하다.   따라서 해당 내용을 구현해줌.
    
    모든 controlleTest
     -   VisitCounterControllerAdvice  component 의 영향은 없지만, 필요한  VisitCounterSerivce 결과를 받아 올 수 있는 GlobalControllerConfig 를 import 하여 test 진행에 문제가 없도록 재설정 해준다.

--- src/test/java/com/fastcampus/projectboardadmin/controller/MainControllerTest.java
+++ src/test/java/com/fastcampus/projectboardadmin/controller/MainControllerTest.java
@@ -1,5 +1,6 @@
 package com.fastcampus.projectboardadmin.controller;
 
+import com.fastcampus.projectboardadmin.config.GlobalControllerConfig;
 import com.fastcampus.projectboardadmin.config.TestSecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,7 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @DisplayName("View Root Controller")
-@Import(TestSecurityConfig.class)
+@Import({TestSecurityConfig.class, GlobalControllerConfig.class})
 @WebMvcTest(MainController.class)
 class MainControllerTest {
 
